c79f5ea6a9444d2276c1c40a338d324cd349eb17
gly <glefranc@eig.fr>
2021-10-01 14:32:05 +0200

    New example based on new tui

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,55 @@
+use crossterm::event::{Event, poll};
+use crossterm::style::Print;
+use crossterm::{cursor, execute};
 use std::io;
-use tui::Terminal;
-use tui::backend::CrosstermBackend;
-use tui::style::{Color, Modifier, Style};
-use tui::widgets::{Block, Borders, Paragraph, Widget, Wrap};
-use tui::text::{Spans, Span};
-use tui::layout::{Alignment, Constraint, Direction, Layout};
+
+mod editor;
+mod tui;
 
 #[cfg(test)]
-mod tests{
+mod tests {
     #[test]
     fn bla() {
-        let a = 647;
-        println!("a: {}", a);
-        println!("a^a: {}", a^a);
-        println!("a^a^a: {}", (a^a)^a);
+        
     }
 }
 
 fn main() -> Result<(), io::Error> {
-    let stdout = io::stdout();
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-    terminal.clear()?;
+    let mut stdout = io::stdout();
 
     let code_rs = std::fs::read_to_string("./src/main.rs")?;
+    
+    let mut frame = tui::Frame::new();
+    let text = "Hello world";
+    let width = tui::terminal_size().0;
+    let (left, right) = (-8, width as i16-2);
+    let (mut pos, mut sens) = ((left,5), true);
+    'edit: loop {
+        use crossterm::event::{read, KeyCode, KeyEvent};
+        if poll(std::time::Duration::from_millis(200))? {
+            match read()? {
+                Event::Key(KeyEvent{ code:KeyCode::Esc, .. }) => break 'edit,
+                // Event::Key(KeyEvent{code: KeyCode::Char(c), ..}) => execute!(stdout, Print(c))?,
+                // Event::Key(KeyEvent{code: KeyCode::Backspace, ..}) => execute!(stdout, cursor::MoveLeft(1), Print(' '), cursor::MoveLeft(1))?,
+                // Event::Mouse(event) => println!("{:?}", event),
+                // Event::Resize(width, height) => println!("New size {}x{}", width, height),
+                _ => ()
+            }
+        }
+        if sens {
+            pos.0+=1;
+        } else {
+            pos.0-=1;
+        }
+        if sens && pos.0 == right {
+            sens=false;
+        } else if  !sens && pos.0 == left {
+            sens=true;
+        }
+        frame.clear()?;
+        frame.write_str(pos, text)?;
+        frame.flush()?;
 
-
-    terminal.draw(|f| {
-        let text = vec![
-            Spans::from(vec![
-                Span::raw("First"),
-                Span::styled("line",Style::default().add_modifier(Modifier::ITALIC)),
-                Span::raw("."),
-            ]),
-            Spans::from(Span::styled("Second line", Style::default().fg(Color::Red))),
-        ];
-        let p = Paragraph::new(text)
-            .block(Block::default().title("Paragraph").borders(Borders::ALL))
-            .style(Style::default().fg(Color::White).bg(Color::Black))
-            .alignment(Alignment::Center)
-            .wrap(Wrap { trim: true });
-
-        let chunks = Layout::default()
-            .direction(Direction::Vertical)
-            .margin(1)
-            .constraints(
-                [Constraint::Ratio(1, 3), Constraint::Ratio(2, 3)].as_ref()
-            )
-            .split(f.size());
-        let block = Block::default()
-             .title("Block")
-             .borders(Borders::ALL);
-        f.render_widget(p, chunks[0]);
-        let block = Block::default()
-             .title("Block 2")
-             .borders(Borders::ALL);
-        f.render_widget(block, chunks[1]);
-    })?;
+    }
     Ok(())
 }
