b104f7555203066783778dd3881f30f88708e33c
gly <glefranc@eig.fr>
2021-10-07 22:58:19 +0200

    Add mod util

--- src/tui/mod.rs
+++ src/tui/mod.rs
@@ -4,35 +4,19 @@ use crossterm::{cursor::MoveTo, queue, style::Print, terminal::{Clear, ClearType
 
 pub mod widget;
 pub mod event_listener;
+pub mod util;
 
-pub struct Rect {
-    pub x: u16,
-    pub y: u16,
-    pub w: u16,
-    pub h: u16,
-}
-pub struct Frame {
-    screen: Rect,
-    term: Stdout
-}
+use util::*;
 
 pub fn terminal_size() -> (u16,u16) {
     use terminal_size::{Height, Width};
     terminal_size::terminal_size().and_then( |(Width(w),Height(h))| Some((w,h))).or_else(|| Some((50,25))).unwrap()
 }
-#[inline]
-fn min<T: PartialOrd>(v1: T, v2: T) -> T {
-    if v1<v2 {v1} else {v2}
-}
-#[inline]
-fn max<T: PartialOrd>(v1: T, v2: T) -> T {
-    if v1>v2 {v1} else {v2}
-}
-#[inline]
-fn clamp<T: PartialOrd>(v_min: T, v_max: T, x: T) -> T {
-    max(v_min, min(v_max, x))
-}
 
+pub struct Frame {
+    screen: Rect,
+    term: Stdout
+}
 
 impl Frame {
     pub fn new() -> Self {
@@ -42,8 +26,11 @@ impl Frame {
             term: std::io::stdout(),
         }
     }
-    pub fn is_in(&self, pos: (i16, i16)) -> bool {
-        let (x,y) = pos;
+    pub fn get_screen(&self) -> Rect {
+        self.screen.clone()
+    }
+    pub fn is_in(&self, pos: Position) -> bool {
+        let Position{x,y} = pos;
         x>=self.screen.x as i16 && x<(self.screen.x+self.screen.w) as i16 && 
         y>=self.screen.y as i16 && y<(self.screen.y+self.screen.h) as i16
     }
@@ -53,25 +40,20 @@ impl Frame {
     pub fn flush(&mut self)  -> Result<(), std::io::Error> {
         self.term.flush()
     }
-    pub fn write_str(&mut self, pos: (i16, i16), text: &str) -> Result<(), std::io::Error>{
-        let (x,y) = pos;
+    pub fn write_str(&mut self, pos: Position, text: &str) -> Result<(), std::io::Error>{
+        let Position{x,y} = pos;
         if y >= self.screen.y as i16 && y < (self.screen.y+self.screen.h) as i16 {
-            let offset = (
-                self.screen.x as i16 - x,
-                (self.screen.x + self.screen.w) as i16 - 1 - x,
-            );
-            let text_offset = (clamp(0, text.len() as i16, offset.0) as usize, clamp(0, text.len() as i16, offset.1) as usize);
-            let text1= &text[text_offset.0..text_offset.1];
-            if text1.len() > 0 {
+            let text = util::cut_text(text, pos.x, (self.screen.x as i16, (self.screen.x + self.screen.w) as i16));
+            if text.len() > 0 {
                 let x1 = clamp(self.screen.x  as i16, (self.screen.x+self.screen.w) as i16, x);
-                return queue!(self.term, MoveTo(x1 as u16, y as u16), Print(text1))
+                return queue!(self.term, MoveTo(x1 as u16, y as u16), Print(text))
             }
-        } 
+        }
         Ok(())
     }
-    pub fn write_char(&mut self, pos: (i16, i16), ch: char) -> Result<(), std::io::Error> {
+    pub fn write_char(&mut self, pos: Position, ch: char) -> Result<(), std::io::Error> {
         if self.is_in(pos) {
-            queue!(self.term, MoveTo(pos.0 as u16, pos.1 as u16), Print(ch))
+            queue!(self.term, MoveTo(pos.x as u16, pos.y as u16), Print(ch))
         } else {
             Ok(())
         }
---
+++ src/tui/util.rs
@@ -0,0 +1,58 @@
+#[derive(Debug,Clone, Copy, Default)]
+pub struct Position {
+    pub x: i16,
+    pub y: i16,
+}
+impl Position {
+    pub fn to_tuple(&self) -> (i16, i16) {
+        (self.x, self.y)
+    }
+}
+
+#[derive(Debug,Clone, Default)]
+pub struct Rect {
+    pub x: u16,
+    pub y: u16,
+    pub w: u16,
+    pub h: u16,
+}
+
+#[inline]
+pub fn min<T: PartialOrd>(v1: T, v2: T) -> T {
+    if v1<v2 {v1} else {v2}
+}
+#[inline]
+pub fn max<T: PartialOrd>(v1: T, v2: T) -> T {
+    if v1>v2 {v1} else {v2}
+}
+#[inline]
+pub fn clamp<T: PartialOrd>(v_min: T, v_max: T, x: T) -> T {
+    max(v_min, min(v_max, x))
+}
+
+pub fn cut_text<'a>(text: &'a str, pos: i16, borders: (i16, i16)) -> &'a str {
+    let offset = (
+        borders.0 - pos,
+        borders.1 - 1 - pos,
+    );
+    let text_offset = (clamp(0, text.len() as i16, offset.0) as usize, clamp(0, text.len() as i16, offset.1) as usize);
+    &text[char_to_utf8_index(text.as_bytes(), text_offset.0)..char_to_utf8_index(text.as_bytes(), text_offset.1)]
+}
+pub fn utf8_len(c: u8) -> usize {
+    if c&0x80 == 0 {1}
+    else if c&0xE0 == 0xC0 {2}
+    else if c&0xF0 == 0xE0 {3}
+    else if c&0xF8 == 0xF0 {4}
+    else {panic!("not utf8 front char")}
+}
+pub fn char_to_utf8_index(s:&[u8], mut ind: usize) -> usize{
+    let len = s.len();
+    let mut i=0;
+    while i < len && ind > 0  {
+        i+=utf8_len(s[i]);
+        ind-=1;
+    }
+    i
+}
+
+pub type TuiResult<T> = Result<T, std::io::Error>;
