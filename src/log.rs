00bf4142f6e331166948b3e684248d7630402a3e
gly <glefranc@eig.fr>
2021-10-13 23:59:01 +0200

    Add a log system output on stdout after leave alternate screen

---
+++ src/log.rs
@@ -0,0 +1,17 @@
+use lazy_static::lazy_static;
+
+use std::{io::*, sync::{Arc, Mutex}};
+
+lazy_static! {
+    pub static ref log: Arc<Mutex<BufWriter<Stdout>>> = Arc::new(Mutex::new(BufWriter::new(stdout())));
+}
+#[macro_export]
+macro_rules! log_writeln {
+    ($($arg:tt)*) => {
+        writeln!($crate::log::log.lock().expect("log lock error"), $($arg)*)
+    };
+}
+
+pub fn flush() -> std::io::Result<()>{
+    log.lock().expect("log lock error").flush()
+}
